c2216d1d5c23bb5877db4cb2238f33b7408c236b
Sherzodjon <124202641+tursunov4@users.noreply.github.com>
2025-12-14 14:35:19 +0500

    dasd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,23 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import ClientLayout from "@/app/clientLayout";
-import {Lato} from "next/font/google";
 import "../assets/css/global.css";
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 
-
-const lato = Lato({
-    weight: ["400", "700"],
-    variable: "--font-lato",
-    subsets: ["latin"],
-    fallback: ["Roboto", "Arial", "sans-serif"]
-});
-
-const siteTitle = process.env.NEXT_PUBLIC_SITE_NAME ?? 'Hunchun';
+const siteTitle = process.env.NEXT_PUBLIC_SITE_NAME ?? "Hunchun";
 
 export const metadata: Metadata = {
-    title: siteTitle,
-    description: `${siteTitle} - информационный портал о Китае`,
+  title: siteTitle,
+  description: `${siteTitle} - информационный портал о Китае`,
 };
 
-export default function RootLayout({children}: Readonly<{ children: ReactNode; }>) {
-    return (
-        <html lang="ru">
-        <body className={`${lato.variable} antialiased`}>
-        <ClientLayout>
-            {children}
-        </ClientLayout>
-        </body>
-        </html>
-    );
+export default function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang="ru">
+      <body className="antialiased">
+        <ClientLayout>{children}</ClientLayout>
+      </body>
+    </html>
+  );
 }
